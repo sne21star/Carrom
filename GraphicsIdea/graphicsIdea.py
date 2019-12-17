b293cf38e621b790698e8a1a971b29e441dd6b3b
snehamahapatra <mahapat0@purdue.edu>
2019-12-16 23:09:55 -0500

    Can't use graphics for some reason look through power point tomorrow

--- GraphicsIdea/graphicsIdea.py
+++ GraphicsIdea/graphicsIdea.py
@@ -4,6 +4,7 @@ win.setCoords(0, 0, 10, 10) # set the coordinates of the window; bottom left is 
 mySquare = Rectangle(Point(1, 1), Point(9, 9)) # create a rectangle from (1, 1) to (9, 9)
 mySquare.draw(win) # draw it to the window
 win.getMouse() # pause before closing
+
 import arcade
 
 # Set constants for the screen size
