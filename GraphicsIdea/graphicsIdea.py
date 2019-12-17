e3d47d1ab3a4511f69e300fd4a82a70b4b9c16f2
snehamahapatra <mahapat0@purdue.edu>
2019-12-16 22:25:39 -0500

    Graphics Files

--- GraphicsIdea/graphicsIdea.py
+++ GraphicsIdea/graphicsIdea.py
@@ -1,4 +1,4 @@
-from graphics import *
+from zellegraphics import *
 win = GraphWin(width = 200, height = 200) # create a window
 win.setCoords(0, 0, 10, 10) # set the coordinates of the window; bottom left is (0, 0) and top right is (10, 10)
 mySquare = Rectangle(Point(1, 1), Point(9, 9)) # create a rectangle from (1, 1) to (9, 9)
